32046c3bec18081ef71105e0c922f5edc5b4b08c
Riski <breezvandalz@gmail.com>
2026-06-25 15:47:48 +0700

    fix: change database config to use safe env variables for linux

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -25,30 +25,23 @@ class Database extends Config
      * @var array<string, mixed>
      */
     public array $default = [
-        'DSN'          => '',
-        'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
-        'DBDriver'     => 'MySQLi',
-        'DBPrefix'     => '',
-        'pConnect'     => false,
-        'DBDebug'      => true,
-        'charset'      => 'utf8mb4',
-        'DBCollat'     => 'utf8mb4_general_ci',
-        'swapPre'      => '',
-        'encrypt'      => false,
-        'compress'     => false,
-        'strictOn'     => false,
-        'failover'     => [],
-        'port'         => 3306,
-        'numberNative' => false,
-        'foundRows'    => false,
-        'dateFormat'   => [
-            'date'     => 'Y-m-d',
-            'datetime' => 'Y-m-d H:i:s',
-            'time'     => 'H:i:s',
-        ],
+        'DSN'      => '',
+        'hostname' => env('DB_HOST', 'localhost'),
+        'username' => env('DB_USER', 'root'),
+        'password' => env('DB_PASS', ''),
+        'database' => env('DB_NAME', 'ci4'),
+        'DBDriver' => 'Postgre', // Kita kunci langsung ke Postgre di sini
+        'DBPrefix' => '',
+        'pConnect' => false,
+        'DBDebug'  => true,
+        'charset'  => 'utf8',
+        'DBCollat' => 'utf8_general_ci',
+        'swapPre'  => '',
+        'encrypt'  => false,
+        'compress' => false,
+        'strictOn' => false,
+        'failover' => [],
+        'port'     => env('DB_PORT', 6543),
     ];
 
     //    /**
