b627b861b08e4c7e218ed78c36190ee560718282
Riski <breezvandalz@gmail.com>
2026-06-25 16:15:19 +0700

    fix: move env function to constructor to prevent php fatal error

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -25,23 +25,30 @@ class Database extends Config
      * @var array<string, mixed>
      */
     public array $default = [
-        'DSN'      => '',
-        'hostname' => env('DB_HOST', 'localhost'),
-        'username' => env('DB_USER', 'root'),
-        'password' => env('DB_PASS', ''),
-        'database' => env('DB_NAME', 'ci4'),
-        'DBDriver' => 'Postgre', // Kita kunci langsung ke Postgre di sini
-        'DBPrefix' => '',
-        'pConnect' => false,
-        'DBDebug'  => true,
-        'charset'  => 'utf8',
-        'DBCollat' => 'utf8_general_ci',
-        'swapPre'  => '',
-        'encrypt'  => false,
-        'compress' => false,
-        'strictOn' => false,
-        'failover' => [],
-        'port'     => env('DB_PORT', 6543),
+        'DSN'          => '',
+        'hostname'     => 'localhost',
+        'username'     => '',
+        'password'     => '',
+        'database'     => '',
+        'DBDriver'     => 'Postgre', // <--- UBAH BAGIAN INI SAJA YA!
+        'DBPrefix'     => '',
+        'pConnect'     => false,
+        'DBDebug'      => true,
+        'charset'      => 'utf8mb4',
+        'DBCollat'     => 'utf8mb4_general_ci',
+        'swapPre'      => '',
+        'encrypt'      => false,
+        'compress'     => false,
+        'strictOn'     => false,
+        'failover'     => [],
+        'port'         => 3306,
+        'numberNative' => false,
+        'foundRows'    => false,
+        'dateFormat'   => [
+            'date'     => 'Y-m-d',
+            'datetime' => 'Y-m-d H:i:s',
+            'time'     => 'H:i:s',
+        ],
     ];
 
     //    /**
@@ -187,9 +194,14 @@ class Database extends Config
     {
         parent::__construct();
 
-        // Ensure that we always set the database group to 'tests' if
-        // we are currently running an automated test suite, so that
-        // we don't overwrite live data on accident.
+        // Memaksa CI4 membaca Environment Variables dari Render
+        $this->default['hostname'] = env('DB_HOST', $this->default['hostname']);
+        $this->default['username'] = env('DB_USER', $this->default['username']);
+        $this->default['password'] = env('DB_PASS', $this->default['password']);
+        $this->default['database'] = env('DB_NAME', $this->default['database']);
+        $this->default['port']     = env('DB_PORT', $this->default['port']);
+
+        // Pastikan database group menjadi 'tests' jika sedang masa testing
         if (ENVIRONMENT === 'testing') {
             $this->defaultGroup = 'tests';
         }
